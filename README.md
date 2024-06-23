1a952eacd99fa6545edfdc921d5cd84a3ddfc3b2
acidicMercury8 <acidicmercury8@outlook.com>
2024-06-23 18:20:11 +0300

    Add files of action

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 Simple action for printing input to output
 
+## Inputs
+
+### `input-string`
+
+- **Required**
+- Input string
+- Default `"input"`
+
+## Outputs
+
+### `output-string`
+
+- Output string
+
 ## License
 
 This project licensed under terms of the __MIT__ license. See [this](./LICENSE) file for details
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try
+{
+    const input = core.getInput("input-string");
+    console.log(`${input}!`);
+
+    const output = input;
+    core.setOutput("output", output);
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`Event payload: ${payload}`);
+}
+catch (error)
+{
+    core.setFailed(error.message);
+}
