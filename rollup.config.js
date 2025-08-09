abf3c16942cf810266e6bf0c845c2c9f86ac48be
acidicMercury8 <acidicmercury8@outlook.com>
2025-08-10 00:56:22 +0300

    Enable bundling licenses

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,13 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import rollupPluginLicense from 'rollup-plugin-license';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const license = rollupPluginLicense;
 
 const config =
 {
@@ -14,8 +22,30 @@ const config =
 
     plugins: [
         commonjs(),
+
         nodeResolve({
             preferBuiltins: true
+        }),
+
+        license({
+            sourcemap: true,
+
+            banner: {
+                content: {
+                    file: join(__dirname, 'LICENSE.txt'),
+                    encoding: 'utf-8',
+                },
+            },
+
+            thirdParty: {
+                includePrivate: true,
+                includeSelf: true,
+                multipleVersions: true,
+                output: {
+                    file: join(__dirname, 'dist', 'LICENSE.txt'),
+                    encoding: 'utf-8',
+                },
+            },
         })
     ]
 };
